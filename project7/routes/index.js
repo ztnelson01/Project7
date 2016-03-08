cdc3fd63f355e432e6f135e9bec2f652f3d84821
Zach Nelson <you@example.com>
2016-03-08 02:34:00 +0000

    trying to commit again

--- project7/routes/index.js
+++ project7/routes/index.js
@@ -7,6 +7,28 @@ router.get('/', function(req, res, next) {
   res.sendFile('Project6.html', { root:  'public' });
 });
 
+router.get('/getCat',function(req,res,next) {
+	res.send('/cat.gif');
+});
+
+router.get('/test2.txt',function(req,res,next) {
+	res.sendFile('test2.txt', {root:	'public'  });
+});
+
+router.get('/test3.gif',function(req,res,next) {
+	res.sendFile('test3.gif', {root:	'public'  });
+});
+
+router.get('/test4.jpg',function(req,res,next) {
+	res.sendFile('test4.jpg', {root:	'public'  });
+});
+
+router.get('/test1.html',function(req,res,next) {
+	res.sendFile('test1.html', {root:	'public'  });
+});
+
+
+
 router.get('/getcity',function(req,res,next) {
             console.log("In getcity route");
 	
